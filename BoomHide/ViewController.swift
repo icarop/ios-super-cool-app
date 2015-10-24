61ff1ef476b73552a2a363b25f85ac379e30e0c0
Icaro Oliveira <icaro@vanics.com>
2015-10-24 16:18:26 -0700

    Many modification

--- BoomHide/ViewController.swift
+++ BoomHide/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var redImage: UIImageView!
+    @IBOutlet weak var blueImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +26,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hideRedButton(sender: AnyObject) {
+        redImage.hidden = true
+    }
+    
+    @IBAction func hideBlueButton(sender: AnyObject) {
+        blueImage.hidden = true
+    }
+    
+    @IBAction func resetButton(sender: AnyObject) {
+        redImage.hidden = false
+        blueImage.hidden = false
     }
 
 
